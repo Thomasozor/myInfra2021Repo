ad94724a158c3a1b4f5f0e0987c2ea3d8fd1fa88
Thomasozor <167851300+Thomasozor@users.noreply.github.com>
2025-01-20 01:21:21 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-state-bucket1"
+    bucket = "my-dev-tf-state-bucket07"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-DynamoDB-table"
+    dynamodb_table = "my-DynamoDB-table07"
   }
 }
