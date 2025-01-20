8da50f4787d6e7495cc2f413d56bce90949d8fa0
Thomasozor <167851300+Thomasozor@users.noreply.github.com>
2025-01-20 01:28:09 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "OZOR1" 
+    default     =  "thomasozor" 
 }
 
 variable "instance_type" { 
